f8eb3e50939c3afb66f8f815d4af45b85b147e42
RioFarhan14 <riofarhan383@gmail.com>
2023-06-01 15:56:07 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::with('category')->get();
+        if (Auth::user()->role->name == 'user') {
+            return view('Product.card', ['products' => $products]);
+        } else {
+            return view('Product.index', ['products' => $products]);
+        }
+    }
+
+    public function create()
+    {
+        $brands = Brand::all();
+        $categories = Category::all();
+
+        return view('product.create', compact('brands', 'categories'));
+    }
+
+    public function store(Request $request)
+    {
+        // ubah nama file
+        $imageName = time() . '.' . $request->image->extension();
+
+        // simpan file ke folder public/product
+        Storage::putFileAs('public/product', $request->image, $imageName);
+
+        $product = Product::create([
+            'category_id' => $request->category,
+            'name' => $request->name,
+            'price' => $request->price,
+            'sale_price' => $request->sale_price,
+            'brands' => $request->brand,
+            'image' => $imageName,
+        ]);
+
+        return redirect()->route('Product.index');
+    }
+
+    public function edit($id)
+    {
+        // ambil data product berdasarkan id
+        $product = Product::where('id', $id)->with('category')->first();
+
+        // ambil data brand dan category sebagai isian di pilihan (select)
+        $brands = Brand::all();
+        $categories = Category::all();
+
+        // tampilkan view edit dan passing data product
+        return view('product.edit', compact('product', 'brands', 'categories'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // cek jika user mengupload gambar di form
+        if ($request->hasFile('image')) {
+            // ambil nama file gambar lama dari database
+            $old_image = Product::find($id)->image;
+
+            // hapus file gambar lama dari folder slider
+            Storage::delete('public/product/' . $old_image);
+
+            // ubah nama file
+            $imageName = time() . '.' . $request->image->extension();
+
+            // simpan file ke folder public/product
+            Storage::putFileAs('public/product', $request->image, $imageName);
+
+            // update data product
+            Product::where('id', $id)->update([
+                'category_id' => $request->category,
+                'name' => $request->name,
+                'price' => $request->price,
+                'sale_price' => $request->sale_price,
+                'brands' => $request->brand,
+                'image' => $imageName,
+            ]);
+        } else {
+            // update data product tanpa menyertakan file gambar
+            Product::where('id', $id)->update([
+                'category_id' => $request->category,
+                'name' => $request->name,
+                'price' => $request->price,
+                'sale_price' => $request->sale_price,
+                'brands' => $request->brand,
+            ]);
+        }
+
+        // redirect ke halaman Product.index
+        return redirect()->route('Product.index');
+    }
+
+    public function destroy($id)
+    {
+        // ambil data product berdasarkan id
+        $product = Product::find($id);
+
+        // hapus data product
+        $product->delete();
+
+        // redirect ke halaman Product.index
+        return redirect()->route('Product.index');
+    }
+}
